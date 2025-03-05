61fc7ea97a427d485bf3811dbde616df0a090123
MD Riaz <mdriaz.wd@gmail.com>
2025-03-05 12:56:26 +0600

    first commit

---
+++ lib/src/event_manager/message_events.dart
@@ -0,0 +1,11 @@
+// Project imports:
+import '../enums.dart';
+import '../message.dart';
+import 'events.dart';
+
+class EventNewMessage extends EventType {
+  EventNewMessage({this.message, this.originator, this.request});
+  dynamic request;
+  Originator? originator;
+  Message? message;
+}
---
+++ lib/src/event_manager/options_events.dart
@@ -0,0 +1,11 @@
+// Project imports:
+import '../enums.dart';
+import '../options.dart';
+import 'events.dart';
+
+class EventNewOptions extends EventType {
+  EventNewOptions({this.message, this.originator, this.request});
+  dynamic request;
+  Originator? originator;
+  Options? message;
+}
